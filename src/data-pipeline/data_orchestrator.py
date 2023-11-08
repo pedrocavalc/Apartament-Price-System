118e5da68fcadf9ae2b11cd5a4891864ad2b6bc4
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-11-08 18:43:41 -0300

    changes in path

--- src/data-pipeline/data_orchestrator.py
+++ src/data-pipeline/data_orchestrator.py
@@ -1,18 +1,19 @@
 from components.get_data import DataManager
 from components.spliter import SpliterDataPipeline
+from components.preprocessor  import PreProcessor
 
 class DataOrchestrator:
     def __init__(self, source, destination):
         pass
 
-    def run(url, path):
+    def run(url, path, id_file):
         data_manager = DataManager(url, path)
-        data_manager.get_data()
-        SpliterDataPipeline().run(path)
+        file_path = data_manager.get_data()
+        SpliterDataPipeline().run(file_path)
+        PreProcessor().run(f'../../data/interim/split_data_{id_file}.csv')
 
-
-def main(url, path):
-    DataOrchestrator.run(url, path)
+def main(url, path, id_file):
+    DataOrchestrator.run(url, path, id_file)
 
 if __name__ == "__main__":
-    main(url="https://archive.ics.uci.edu/static/public/555/apartment+for+rent+classified.zip", path="../../../data/raw/")
+    main(url="https://archive.ics.uci.edu/static/public/555/apartment+for+rent+classified.zip", path="../../data/raw", id_file=0)
