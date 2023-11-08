57b3c6da1949668d54e69327fed4d6a768978c4e
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-11-08 11:08:20 -0300

    create oirchestrator

--- src/data-pipeline/data_orchestrator.py
+++ src/data-pipeline/data_orchestrator.py
@@ -1,7 +1,18 @@
+from components.get_data import DataManager
+from components.spliter import SpliterDataPipeline
+
 class DataOrchestrator:
     def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
-
-    def run(self):
         pass
+
+    def run(url, path):
+        data_manager = DataManager(url, path)
+        data_manager.get_data()
+        SpliterDataPipeline().run(path)
+
+
+def main(url, path):
+    DataOrchestrator.run(url, path)
+
+if __name__ == "__main__":
+    main(url="https://archive.ics.uci.edu/static/public/555/apartment+for+rent+classified.zip", path="../../../data/raw/")
