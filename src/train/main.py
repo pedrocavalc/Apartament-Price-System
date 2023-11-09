1fb8ad15c3213a0e338a97383b7d9ee326d9acaa
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-11-09 18:45:15 -0300

    create train orchestration

--- src/train/main.py
+++ src/train/main.py
@@ -2,12 +2,13 @@ from trainer.trainer_orchestrator import TrainerOrchestrator
 
 
 
-def main(data_path, target):
-    trainer = TrainerOrchestrator(data_path, target)
+def main(data_path, target, columns = None):
+    trainer = TrainerOrchestrator(data_path, target,columns= columns)
     trainer.run()
 
 
 
 
 if __name__ == "__main__":
-    main(data_path="../../data/interim/split_data_0.csv", target="price")
+    columns = ["currency","fee","pets_allowed","category","cityname","price_type","state","bathrooms","bedrooms","square_feet","price"]
+    main(data_path="../../data/interim/split_data_0.csv", target="price", columns = columns)
--- src/train/trainer/trainer_orchestrator.py
+++ src/train/trainer/trainer_orchestrator.py
@@ -1,16 +1,32 @@
 from trainer.components.dataloader.dataloader import DataLoader
+from trainer.components.models.modelsManager.models_manager import ModelsManager
+from trainer.components.models.pipeline.custom_pipeline import CustomPipeline
 
 class TrainerOrchestrator():
-    def __init__(self, data_path, target):
+    def __init__(self, data_path, target, columns):
         self.data_path = data_path
         self.target = target
+        self.columns= columns
 
     def run(self):
-        data_loader = self.get_data()
-        self.train(data_loader)
+        data_loader = self.get_data_loader()
+        models_to_train = self.get_models()
+        self.train(data_loader,models_to_train)
+
     
-    def get_data(self):
-        data_loader = DataLoader(self.data_path, self.target)
+    def get_data_loader(self):
+        data_loader = DataLoader(self.data_path, self.target,self.columns)
         data_loader.run()
         return data_loader
     
+    def get_models(self):
+        models_to_train = ModelsManager().get_models_dict()
+        return models_to_train
+
+    def train(self, data_loader,models_to_train):
+        for model_name, model in models_to_train.items():
+            model = CustomPipeline().build_model(model, data_loader.get_categorical_columns(), data_loader.get_numerical_columns())
+            print(data_loader.get_train_data()[0].isnull().sum())
+            model.fit(data_loader.get_train_data()[0], data_loader.get_train_data()[1])
+
+
