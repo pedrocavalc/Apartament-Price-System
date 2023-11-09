87d3ccb0bbafa9cafd1b76233a17325218233737
pedrocavalc <pedro.cavalvante@lapisco.ifce.edu.br>
2023-11-09 18:45:33 -0300

    create manager for models

---
+++ src/train/trainer/components/models/modelsManager/models_manager.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
+
+class ModelsManager():
+    def __init__(self) -> None:
+        self.model_dict = {"LinearRegression": LinearRegression(), 
+                           "LogisticRegression": LogisticRegression(),
+                           "RandomForestRegressor": RandomForestRegressor(), 
+                           "MLPRegressor": MLPRegressor(),}
+    def get_models_dict(self):
+        return self.model_dict
